d36e3d676fcdf85ad5a1d9833580681300d9e7e6
BaylorRae <baylorrae@gmail.com>
2010-08-15 12:07:37 -0500

    No longer deletes index.php

--- clear_cache.php
+++ clear_cache.php
@@ -4,7 +4,8 @@ $dir = dirname(__FILE__) . '/cache/*';
 
 // Open a known directory, and proceed to read its contents  
 foreach(glob($dir) as $file)   {  
-  unlink($file);
+  if( !preg_match('/index\.php/', $file) )
+    unlink($file);
 }
 
 header('Location: /wp-admin/admin.php?page=wufoo-phooey');
